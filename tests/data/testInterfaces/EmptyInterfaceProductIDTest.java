21117cbc1303e1e243d60ea01b4c89c9a367bdd4
jnoguero <jnd3@alumnes.udl.cat>
2021-01-13 01:57:25 +0100

    Program's testing done

---
+++ tests/data/testInterfaces/EmptyInterfaceProductIDTest.java
@@ -0,0 +1,9 @@
+package data.testInterfaces;
+
+import exceptions.InvalidUPCFormat;
+import org.junit.jupiter.api.Test;
+
+public interface EmptyInterfaceProductIDTest {
+    @Test
+    void EmptyProductIDTest() throws InvalidUPCFormat;
+}
